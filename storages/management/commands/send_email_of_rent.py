97224a554f9e1401b2fa1e84bde81b0a26d561fe
b10t <b10t.rus@gmail.com>
2022-06-20 21:47:27 +0800

    Send email of rent.

--- storages/management/commands/send_email_of_rent.py
+++ storages/management/commands/send_email_of_rent.py
@@ -1,27 +1,40 @@
-from email.policy import default
-
-from django.core.mail import EmailMessage
+from django.conf import settings
+from django.core.mail import send_mail
 from django.core.management.base import BaseCommand
+from storages.models import Rent
 
 
-def send_email_of_rent():
-    # subject_message = 'Вы успешно зарегестрированы на CakeBake'
-    # message = f'''
-    #         Чтобы войти пройдите по ссылке: https://{current_site.domain}/users/login/
-    #         Ваш логин: {email}
-    #         Ваш пароль: {password}
-    #         '''
-    # EmailMessage(
-    #     subject=subject_message,
-    #     body=message,
-    #     to=[email],
-    # ).send()
+def send_email_of_rent(rent, auth_user, auth_password):
+    subject_message = 'У вас заканчивается срок аренды!'
+    message = f'''
+            Добрый день.
 
-    pass
+            У Вас заканчивается срок аренды бокса:
+
+            Город: {rent.box.storage.city}
+            Адрес: {rent.box.storage.address}
+            № бокса: {rent.box.number}
+            '''
+    send_mail(
+        subject=subject_message,
+        message=message,
+        recipient_list=[rent.tenant.email],
+        from_email=auth_user,
+        auth_user=auth_user,
+        auth_password=auth_password
+    )
 
 
 class Command(BaseCommand):
     help = "Send email of rent."
 
     def handle(self, *args, **options):
-        print('send email')
+        auth_user = settings.EMAIL_HOST_USER
+        auth_password = settings.EMAIL_HOST_PASSWORD
+
+        for rent in Rent.objects.all():
+            send_email_of_rent(
+                rent,
+                auth_user,
+                auth_password
+            )
